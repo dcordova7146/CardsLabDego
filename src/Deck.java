564d3f07d2b4207d4ede926a702f219870286871
dego <dcordova7146>
2018-12-11 10:15:05 -0500

    day 2

--- src/Deck.java
+++ src/Deck.java
@@ -2,11 +2,26 @@ import java.util.ArrayList;
 
 public class Deck {
     private ArrayList<Card> unDealt;
-    private ArrayList<Card> Dealt;
-
+    private ArrayList<Card> dealt;
+    private int size;
     public Deck(String[] ranks, String[] suits, int[] pointValues){
-        for(int i = 0;i<52;i++){
-            this.unDealt.add(i,)
+        unDealt = new ArrayList<Card>();
+        dealt = new ArrayList<Card>();
+        for(int i = 0;i<suits.length;i++) {
+            for (int j = 0; j < ranks.length; j++) {
+                Card tempCard = new Card(ranks[j], suits[i], pointValues[j+1]);
+                this.unDealt.add(tempCard);
+            }
         }
+    }
+    public boolean isEmpty(){
+        return unDealt.isEmpty();
+    }
+    public int Size(){
+        return size;
+    }
+    public Card deal(){
+        dealt.add(unDealt.remove(0));
+
     }
 }
